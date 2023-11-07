619d25f254a21e1b41616c89a6639eb4651eb9bb
Karl Minton <kminton@frontier.co.uk>
2023-11-07 16:37:06 +0000

    added some basic elements for testing / debugging

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const scene = new THREE.Scene();
 const loader = new FBXLoader();
 loader.load('static/swat.fbx', (o) => scene.add(o))
 
+// TODO fix magic numbers
 const camera = new THREE.PerspectiveCamera(
     50,
     window.innerWidth / window.innerHeight,
@@ -20,12 +21,28 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+// TODO abstract making a map
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 0.2;
 scene.add(cube);
 
-camera.position.z = 5;
+const light = new THREE.AmbientLight(0x404040);
+scene.add(light);
+
+const planeGeometry = new THREE.PlaneGeometry(20, 20);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xffdda0000})
+planeMaterial.side = THREE.DoubleSide
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.z = 8;
+plane.position.y = -0.55;
+plane.rotation.x-=1.55;
+
+
+scene.add(plane);
+
+camera.position.z = 10;
 
 const controller = new FirstPersonCameraController(camera);
 
